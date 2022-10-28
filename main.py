5cd8c49015120e4171807d1b56a412455f4e0ec2
Matteo Bini <mail@matteobini.me>
2022-10-28 07:30:28 +0200

    ,

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
         pending_registration(subs)  # Send the confirmation email
         welcome_notification(subs)  # Welcome the new users
 	try:
-            register_new_telegram_user(subs)
+        register_new_telegram_user(subs)
 	except:
 	    pass
         
